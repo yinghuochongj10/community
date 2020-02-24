647537253b55fc0fe71fce33ba4ca35e33486fda
yinghuochongj10 <1135798370@qq.com>
2020-02-24 16:06:35 +0800

    s

--- src/main/java/com/cy/CommunityApplication.java
+++ src/main/java/com/cy/CommunityApplication.java
@@ -1,9 +1,11 @@
 package com.cy;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan(basePackages = "com.cy.mapper")
 public class CommunityApplication {
 
 	public static void main(String[] args) {
